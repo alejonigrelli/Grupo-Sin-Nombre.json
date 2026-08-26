2874f410e4c7f55c0b2632f1c6599958c36dc172
Alejo Nigrelli <133166719+alejonigrelli@users.noreply.github.com>
2026-08-26 18:39:38 -0300

    Delete Semana 1 directory

--- Semana 1/ejercicio 1/script.js
+++
@@ -1,8 +0,0 @@
-const precioBaseSilla = 4500
-const iva = 0.21
-const valorIva = precioBaseSilla * iva
-const precioFinal = precioBaseSilla + valorIva
-
-console.log(`Precio Base de la Silla: $${precioBaseSilla}`)
-console.log(`IVA: $${valorIva}`)
-console.log(`Precio final: $${precioFinal}`)
